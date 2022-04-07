dd8aab203270ea064921f6be14abfb1f21f190ea
khyan22 <khyanalvarez@gmail.com>
2022-04-07 03:28:26 -0400

    added color warning

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,13 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
+  //this is where date goes
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
+  //this is where task text goes
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +17,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -126,6 +132,7 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus")
 });
 
+
 //changes <textarea> back into a <span>
 $(".list-group").on("change", "input[type='text']", function() {
   //get current value
@@ -156,6 +163,7 @@ $(".list-group").on("change", "input[type='text']", function() {
   //replace input with span element
   $(this).replaceWith(taskSpan);
 });
+
 
 //makes <ul> sortable
 $(".card .list-group").sortable({
@@ -226,11 +234,33 @@ $("#trash").droppable({
   }
 });
 
+//!change back to "1"
 //adds a calender to the modal date input that the user can pick form
 $("#modalDueDate").datepicker({
-  minDate: 1
+  minDate: -1
 });
 
+
+// color codes task depending on time until due date
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl).find("span")
+    .text()
+    .trim();
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  //remove any old class from element 
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning")
+  }
+}
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -266,6 +296,31 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+// value of due date was changed
+$(".list-group").on("change", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    $(this).replaceWith(taskSpan);
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
